bedcc1ef6fc7a2b8136ed84f6b48cfe5eb8c2cf3
Dhanush Pugazhendhi <118467512+Dhanushpugazh@users.noreply.github.com>
2025-05-25 18:44:35 +0530

    Update login.js

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -71,7 +71,7 @@ function Login({ onLogin }) {
     setError(''); // reset error before trying login
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://military-asset-management-fy15.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
