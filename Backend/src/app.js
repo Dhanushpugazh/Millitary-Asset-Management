0e770209fb36b3802ed1c34b8f0eb8b73fffdf0e
Dhanush Pugazhendhi <dhanushpugazhendhi7@gmail.com>
2025-05-25 18:28:10 +0530

    third commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
 module.export=app;
